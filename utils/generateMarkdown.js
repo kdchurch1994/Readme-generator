38ca2444b5da116f8fdedbe9ef5eebf90bfe0687
kdchurch1994 <kdchurch@bsu.edu>
2021-08-24 02:25:43 -0400

    function to render license badge is complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache-2.0") {
+    return `[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if (license === "MPL-2.0") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licences/MPL-2.0)`
+  }
+  else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licences/MIT)`
+  }
+  else {
+    return "";
+  }
+
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
