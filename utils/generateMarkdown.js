c5a3c612604f69325c2e56319c958c0250378e67
kdchurch1994 <kdchurch@bsu.edu>
2021-08-24 02:37:33 -0400

    render license link function complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,7 +18,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Apache-2.0") {
+    return `[Apace-2.0 Link](https://opensource.org.licenses/Apache-2.0)`
+  }
+  else if (license === "MPL-2.0") {
+    return `[MPL-2.0 Link](https://opensource.org/licenses/MPL-2.0)`
+  }
+  else if (license === "MIT") {
+    return `[MIT Link](https://opensource.org/licenses/MIT)`
+  }
+  else {
+    return "";
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
